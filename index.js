686171efa447ae844feaefba15cb795dec08cab5
Teejay Dixon <tahjgdixon@gmail.com>
2022-08-14 23:38:35 -0700

    commit tree a708eaf91d89b5cfec95a42321806567b64ae52e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+
+
+
+/*function writeCards(object, key) {
+    let myVar = [];
+
+    for (i = 0; i < myVar.length; i++){
+      let obj = (`Thank you, ${myVar[i]}, for the wonderful ${key} gift!`);
+       
+  };
+  return console.log(obj);
+}; 
+
+
+writeCards(['Guadalupe', 'Aki', 'Ollie'], 'birthday'); */
+let time = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+function countDown() {
+  
+  for (let i = 0; i < time.length; time--){
+    
+    console.log(time[i]);
+  }
+
+};
+
+countDown();
